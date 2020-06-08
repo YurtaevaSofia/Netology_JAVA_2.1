6ae6ed316f27a53f546632819c9a53d06f695981
coursar <Sonia@MacBook-Air-Sofa.local>
2020-06-08 13:20:38 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int currentBalance = 2000000000;
+        int transferSumm = 500000000;
+        currentBalance += transferSumm;
+
+        System.out.println(currentBalance);
+    }
+}
